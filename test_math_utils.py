e5150c225cf0fd6c9a370bd203750f1102e80f39
vishvajit_9520 <vishwjits724@gmail.com>
2026-04-10 22:36:24 +0530

    Create test_math_utils.py

---
+++ test_math_utils.py
@@ -0,0 +1,10 @@
+import unittest
+from math_utils import add
+
+class TestMathUtils(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 6)  # ❌ Wrong expected value (intentional bug)
+
+if __name__ == '__main__':
+    unittest.main()
