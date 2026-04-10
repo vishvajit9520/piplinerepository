00308a57f0661ec671496645eded5ee740ec6309
vishvajit_9520 <vishwjits724@gmail.com>
2026-04-11 00:16:35 +0530

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -4,6 +4,5 @@ from math_utils import add
 class TestMathUtils(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)  # ✅ correct
-if __name__ == '__main__':
+        self.assertEqual(add(2, 3), 6)if __name__ == '__main__':
     unittest.main()
