0e7ade40d39b97dca8a71cb82acc3eb8c6e21249
vishvajit_9520 <vishwjits724@gmail.com>
2026-04-11 22:40:52 +0530

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,8 +1,10 @@
 import unittest
 from math_utils import add
 
-class TestMathUtils(unittest.TestCase):
+class TestMath(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 6)if __name__ == '__main__':
+        self.assertEqual(add(2, 3), 6)  # ❌ Wrong expected value (bug)
+
+if __name__ == '__main__':
     unittest.main()
